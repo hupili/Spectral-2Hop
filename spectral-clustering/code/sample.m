575ccaa9f16a90cc7d6a38c9f0197546b8f0483b
hpl <hupili@ie.cuhk.edu.hk>
2012-05-14 22:09:18 +0800

    add kmeans result on polar system

--- spectral-clustering/code/sample.m
+++ spectral-clustering/code/sample.m
@@ -54,3 +54,19 @@ print(gcf, '-dpng', '../plot/sc_sample_2d_embed.png') ;
 % set(h, 'LineWidth', 20) ;
 % h = plot(c(2, 1), c(2, 2), '*r') ;
 % set(h, 'LineWidth', 20) ;
+
+figure ;
+%Test polar coordinate system
+%1. subtract sample mean 
+%2. convert from cartesian coordinate to polar coordinate
+[ptheta, pr] = cart2pol(X(:,1) - mean(X(:,1)), X(:,2) - mean(X(:,2))) ;
+PX = [zscore(ptheta), zscore(pr)] ;
+[idx, c] = kmeans(PX, K) ;
+plot(X(idx==1, 1), X(idx==1, 2), 'ob') ;
+hold on ;
+plot(X(idx==2, 1), X(idx==2, 2), 'or') ;
+% h = plot(c(1, 1), c(1, 2), '*b') ;
+% set(h, 'LineWidth', 20) ;
+% h = plot(c(2, 1), c(2, 2), '*r') ;
+% set(h, 'LineWidth', 20) ;
+print(gcf, '-dpng', '../plot/sc_sample_pkmeans.png') ;
